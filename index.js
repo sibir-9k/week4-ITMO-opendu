c881e3721d22a7e997e5be219f0112ed5ded7302
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2021-10-08 17:02:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import http from 'http';
+const CORS = {
+   'Access-Control-Allow-Origin': '*',
+   'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+   'Access-Control-Allow-Headers': 'Content-Type,Accept,Access-Control-Allow-Headers, x-test'
+};
+
+const server = http.createServer(async(req, res) => {
+   if(req.url === '/result4/') {
+      res.writeHead(200, {
+         'Content-Type': 'application/json',
+         ...CORS,
+      })
+
+      let data = '';
+      await req.on('data', function(chunk) {
+         data += chunk;
+         }).on('end', () => {
+      })
+      res.write(JSON.stringify({
+         "message":"id71293382",
+         "x-result":headers,
+         "x-body":data
+      }))
+   }
+   res.end()
+});
+
+server.listen(4321,() => {
+   console.log('Server is running')
+});
