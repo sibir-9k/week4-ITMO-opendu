3e7edeb467be1b31303e2ed632abbe9a21cb260f
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2021-10-09 21:46:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-import http from 'http';
-const CORS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test'
-  };
-const server = http.createServer(async(req,res)=>{
-      if(req.url === '/result4/'){
-          
-          res.writeHead(200,{
-              'Content-Type':'application/json',
-              ...CORS,
-          })
-          let data = '';
-          await req.on('data', function(chunk){
-            data += chunk;
-            }).on('end', () => {
-          })
-          res.write(JSON.stringify({
-            "message":"id71293382",
-            "x-result":headers,
-            "x-body":data
-            }
-            ))
-      }
-      res.end()
-      
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors');
+
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','x-test');
+    res.setHeader('Content-Type', 'application/json');
+    next();
   });
-  server.listen(4321,()=>{
-      console.log('Server is running')});
+
+app.use(bodyParser.urlencoded({extended : false}));
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+app.use(express.json())
+app.use(bodyParser.text());
+app.options('*', cors());
+
+app.get('/result4/', (req, res) => {
+    res.json({
+        message: 'id71293382',
+        'x-body': req.body,
+        'x-result': req.header('x-test')
+    });
+});
+
+app.post('/result4/', (req, res) => {
+    res.json({
+        message: 'id71293382',
+        'x-body': req.body,
+        'x-result': req.header('x-test')
+    });
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('server running on port 3000', '');
+});
