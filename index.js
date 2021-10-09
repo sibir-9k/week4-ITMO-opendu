8a68f8bac5cf0e47b0eb233c0af25239a02c75a0
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2021-10-09 21:50:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const cors = require('cors');
-
-
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers','x-test');
-    res.setHeader('Content-Type', 'application/json');
-    next();
-  });
-
-app.use(bodyParser.urlencoded({extended : false}));
-
-app.use(
-    cors({
-        credentials: true,
-        origin: true
-    })
-);
-app.use(express.json())
-app.use(bodyParser.text());
-app.options('*', cors());
-
-app.get('/result4/', (req, res) => {
-    res.json({
-        message: 'id71293382',
-        'x-body': req.body,
-        'x-result': req.header('x-test')
-    });
-});
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+const app = express()
+app.use(bodyParser.text())
+app.use(cors())
 app.post('/result4/', (req, res) => {
-    res.json({
-        message: 'id71293382',
-        'x-body': req.body,
-        'x-result': req.header('x-test')
-    });
-});
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log('server running on port 3000', '');
-});
+    const xTest = req?.headers?.['x-test']
+    const xBody = req?.body
+    res.json({ message: 'id71293382', "x-result": xTest, "x-body": xBody })
+})
+app.get('/result4/', (req, res) => {
+    const xTest = req?.headers?.['x-test']
+    const xBody = 'abc'
+    res.json({ message: 'id71293382', "x-result": xTest, "x-body": xBody })
+})
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`))
