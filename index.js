0e29525a736f38e17c1f6fa3d78200054b359db1
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2021-10-31 17:36:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const PORT = process.env.PORT || 5000
 const app = express()
-app.use(bodyParser.json().text())
+app.use(bodyParser.text())
+app.use(bodyParser.json())
 app.use(cors())
 app.post('/result4/', (req, res) => {
     const xTest = req?.headers?.['x-test']
