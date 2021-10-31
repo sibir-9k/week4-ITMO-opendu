a4511919303aa257cb25092ccb217d5fa691e34a
Egor Frolov <79311642+sibir-9k@users.noreply.github.com>
2021-10-31 20:11:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ http
     if (req.url === '/result4/') {
       let CORS = {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-        'Access-Control-Allow-Headers':
-          'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'x-test,Content-Type,Accept,Access-Control-Allow-Headers'
+
       };
 
       const result = {
